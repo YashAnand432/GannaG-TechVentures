9f7f6d2ff694811a31cbbe0db6ed70c9bb4f2f87
Yash Anand 432 <YashAnand432@users.noreply.github.com>
2025-03-20 23:48:07 +0530

    Fix : 1. Removed initial Next js layout. 2. Initialised a landing page, that will be the root url. 3. Initialised a /home route that is the home page(page after loggin in or signing in). 3. Set up a Next.js Layout for the home page. Left side is the navbar and right side is the placeholder.

---
+++ app/home/alltools/page.tsx
@@ -0,0 +1,7 @@
+export default function mycontent() {
+    return (
+        <div>
+            all tools
+        </div>
+    )
+}
---
+++ app/home/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function mycontent() {
+    return (
+        <div>
+            my dashboard
+        </div>
+    )
+}
